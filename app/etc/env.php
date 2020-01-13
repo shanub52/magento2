9a62becca8c6f736fdc3fbe2f9fc5f07b02d709b
surender <shanub52@gmail.com>
2020-01-13 14:23:38 +0530

    first and commit

---
+++ app/etc/env.php
@@ -0,0 +1,65 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'adminlogin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '0aa616bff763aa5e522a4808ef9627f1
+biY8vdWx4w8KV5Q59380Fejy36l6ssUb
+biY8vdWx4w8KV5Q59380Fejy36l6ssUb
+biY8vdWx4w8KV5Q59380Fejy36l6ssUb',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magentodb',
+        'username' => 'magentouser',
+        'password' => 'Magento123@',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sun, 12 Jan 2020 15:52:03 +0000',
+  ),
+);
