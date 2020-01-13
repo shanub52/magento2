17f1771c14e90b3c71237d71a9f60a09bb38d16a
surender <shanub52@gmail.com>
2020-01-13 13:48:10 +0530

    delete all

--- dev/tests/static/framework/tests/unit/testsuite/Magento/TestFramework/Inspection/_files/words_finder/interview_with_the_vampire.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-echo 'A man tells his epic life story: love, betrayal, loneliness, and hunger.';
